adaa4603678b9beac316397d916e4f92a026c57e
Yonatan Verhskov <yav2002>
2020-11-11 15:57:06 +0200

    sapareted file

--- light_and_matter.py
+++ light_and_matter.py
@@ -13,8 +13,8 @@ def throw_photon(L, l_mean, p_abs):
     cur_theta = 0
     while True:
         cur_x, cur_x, length = get_next_loc(cur_x, cur_y, cur_theta, l_mean)
-        what_happens = which_event_uniform(L, l_mean, p_abs);
-        if in_which_block(cur_y) == -2
+        what_happens = which_event_uniform(L, l_mean, p_abs)
+        if in_which_block(cur_y) == -2:
             return "Reflected"
         elif in_which_block(cur_y) == -1:
             return "Transmitted"
