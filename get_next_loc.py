bfd7f7d8a41c6cc644af205ab77d31c639d3d607
Yonatan Verhskov <yav2002>
2020-11-11 15:18:09 +0200

    fixes

--- get_next_loc.py
+++ get_next_loc.py
@@ -3,7 +3,8 @@ from math import *
 
 
 def get_next_loc(current_x, current_y, theta, l_mean):
+    """returns the next location of proton"""
     l = np.random.exponential(l_mean, 1)
     new_y = l * sin(theta) + current_x
     new_x = l * cos(theta) + current_y
-    return new_x, new_y
+    return new_x, new_y, l
