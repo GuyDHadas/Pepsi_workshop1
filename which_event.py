5c0c70fdf91cf2f1d5d986ded12ed42767c66680
Shadowguy1809 <guy.hadas01@gmail.com>
2020-11-11 15:14:01 +0200

    a function that chooses the next interaction

---
+++ which_event.py
@@ -0,0 +1,13 @@
+import numpy as np
+from consts import *
+
+def which_event_uniform(p_abs, mu_sca, length):
+    rnd_uni = np.random.uniform()
+    if rnd_uni < p_abs:
+        return "abs"
+    elif rnd_uni < p_abs + mu_sca * length:
+        return "sca"
+    else:
+        return ""
+
+print(which_event_uniform(p_abs, mu_sca, 1))
