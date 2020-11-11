95050fb236fe24f562b4ae95777991c6c005ea03
Shadowguy1809 <guy.hadas01@gmail.com>
2020-11-11 15:51:55 +0200

    string

--- light_and_matter.py
+++ light_and_matter.py
@@ -4,35 +4,36 @@ import numpy as np
 from get_next_loc import *
 from which_event import *
 from consts import *
-from matplotlib import pyplot as plt
+from check_in_block import in_which_block
 
 
 def throw_photon(L, l_mean, p_abs):
-    p = [0,0]
-    theta = 0
-
+    cur_x = 0
+    cur_y = 0
+    cur_theta = 0
     while True:
-        p[0], p[1], length = get_next_loc(p[0], p[1], theta, l_mean)
+        cur_x, cur_x, length = get_next_loc(cur_x, cur_y, cur_theta, l_mean)
         what_happens = which_event_uniform(L, l_mean, p_abs);
-        if p[0] <= 0:
-            return 0
-        if p[0] >= L:
-            return -1
+        if in_which_block(cur_y) == -2
+            return "Reflected"
+        elif in_which_block(cur_y) == -1:
+            return "Transmitted"
         if what_happens == 'abs':
-            return 1
+            return "Absorbed"
         if what_happens == 'sca':
-            theta = np.random.uniform(0, 2 * math.pi)
+            cur_theta = np.random.uniform(0, 2 * math.pi)
 
 def simulation():
     results = []
-
     L = 5
     l_mean = 3
     p_abs = 1/3
+    mu_sca = 2/9
+    for i in range (0,10**5):
+        results.append(throw_photon)
+    pass
 
-    for i in range(0, 10**5):
-        results.append(throw_photon(L, l_mean, p_abs))
 
-    plt.figure()
-    plt.bar(results)
-    plt.show()
+
+print(throw_photon(blocks[0], l_mean, p_abs))
+
