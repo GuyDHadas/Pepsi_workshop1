e7c1f2dd92d7cddcb93fa3ab242d05bb73a646f6
Shadowguy1809 <guy.hadas01@gmail.com>
2020-11-11 16:18:09 +0200

    which

--- which_event.py
+++ which_event.py
@@ -2,14 +2,14 @@ import numpy as np
 from consts import *
 
 
-def which_event_uniform(length, l_mean, p_abs):
+def which_event_uniform(length, cur_block, l_mean, p_abs):
 
-    mu_sca = (1-p_abs)/l_mean
+    mu_sca = (1-p_abs[cur_block])/l_mean[cur_block]
 
     rnd_uni = np.random.uniform()
-    if rnd_uni < p_abs:
+    if rnd_uni < p_abs[cur_block]:
         return "abs"
-    elif rnd_uni < p_abs + mu_sca * length:
+    elif rnd_uni < p_abs[cur_block] + mu_sca * length[cur_block]:
         return "sca"
     else:
         return ""
