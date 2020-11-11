9ec5d313c582723c83d841a3c658552dbae3dc1b
Shadowguy1809 <guy.hadas01@gmail.com>
2020-11-11 15:58:43 +0200

    stringy

--- light_and_matter.py
+++ light_and_matter.py
@@ -12,7 +12,7 @@ def throw_photon(L, l_mean, p_abs):
     cur_y = 0
     cur_theta = 0
     while True:
-        cur_x, cur_x, length = get_next_loc(cur_x, cur_y, cur_theta, l_mean)
+        cur_x, cur_y, length = get_next_loc(cur_x, cur_y, cur_theta, l_mean)
         what_happens = which_event_uniform(L, l_mean, p_abs)
         if in_which_block(cur_y) == -2:
             return "Reflected"
